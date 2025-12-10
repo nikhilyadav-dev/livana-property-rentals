ea4ef03860478825267ff3e99e55e60f343203e1
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2025-12-10 12:52:05 +0530

    Added Slide Image Functionality

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,11 +1,34 @@
 let images = window.sliderImages;
 let imageIndex = null;
 
-const fullSlider = document.getElementById("fullSlider");
+const fullSlider = document.querySelector(".fullSlider");
 const fullImage = document.getElementById("fullImage");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 const closeSlider = document.getElementById("closeSlider");
 
 console.log(images);
-console.log(fullSlider);
+
+function openSlider(index) {
+  console.log(index);
+  imageIndex = index;
+  fullImage.src = images[imageIndex].url;
+  fullSlider.classList.remove("hidden");
+}
+
+// Next image
+nextBtn.onclick = () => {
+  imageIndex = (imageIndex + 1) % images.length;
+  fullImage.src = images[imageIndex].url;
+};
+
+// Prev image
+prevBtn.onclick = () => {
+  imageIndex = (imageIndex - 1 + images.length) % images.length;
+  fullImage.src = images[imageIndex].url;
+};
+
+// Close slider
+closeSlider.onclick = () => {
+  fullSlider.classList.add("hidden");
+};
