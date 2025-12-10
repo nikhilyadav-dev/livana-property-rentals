82b59844f634868806dfec870fd68b744c1ee89e
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2025-12-10 12:11:44 +0530

    Refactor Show Lisitng Page Image UI

---
+++ public/js/slider.js
@@ -0,0 +1,11 @@
+let images = window.sliderImages;
+let imageIndex = null;
+
+const fullSlider = document.getElementById("fullSlider");
+const fullImage = document.getElementById("fullImage");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const closeSlider = document.getElementById("closeSlider");
+
+console.log(images);
+console.log(fullSlider);
