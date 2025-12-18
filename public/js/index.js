747429837d5e579e8dd295b9f7a1cd73c3e02c21
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2025-12-18 15:24:41 +0530

    Added Display Price After Tax Logic

--- public/js/index.js
+++ public/js/index.js
@@ -73,3 +73,39 @@ function prevSlide(e, btn) {
 function goToListing(id) {
   window.location.href = `/listings/${id}`;
 }
+
+// Show Text Price
+
+const gstToggle = document.querySelector(".gstToggle");
+const priceEls = document.querySelectorAll(".price-info");
+const taxEls = document.querySelectorAll(".tax-info");
+const changeAfter = document.querySelectorAll(".changeAfter");
+console.log(priceEls);
+
+const GST_RATE = 0.18;
+
+gstToggle.addEventListener("change", () => {
+  priceEls.forEach((priceEl, index) => {
+    const basePrice = Number(priceEl.dataset.price);
+    console.log(basePrice);
+    const gstPrice = Math.round(basePrice + basePrice * GST_RATE);
+
+    console.log(gstPrice);
+
+    if (gstToggle.checked) {
+      priceEl.style.display = "none";
+      taxEls[index].style.display = "inline";
+      taxEls[index].style.fontWeight = "600";
+      taxEls[index].innerText = `₹${gstPrice} / night`;
+    } else {
+      priceEl.style.display = "inline";
+      taxEls[index].style.display = "none";
+    }
+  });
+
+  changeAfter.forEach((el) => {
+    el.innerText = gstToggle.checked
+      ? "Display total after taxes"
+      : "Display total before taxes";
+  });
+});
